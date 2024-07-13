77febb3945a6fced5ff1446f2a807ea0f0ee5bd9
Loghi-GenAI <loghi.etl@gmail.com>
2024-07-13 09:04:46 +0530

    Update tweetgen.py

--- tweetgen.py
+++ tweetgen.py
@@ -8,9 +8,9 @@ st.title("Tweet Generator")
 st.subheader("Generate Tweets on any topic")
 topic = st.text_input("Topic")
 number = st.number_input("No. of tweets",min_value=1,max_value=10,step=1,value=1)
+gemini_model = CGGA(model="gemini-1.5-flash-latest")
 
 if st.button("Generate"):
     prompt = f"Give me {number} of tweets on {topic}"
-    gemini_model = CGGA(model="gemini-1.5-flash-latest")
     response=gemini_model.invoke(prompt)
     st.write(response.content)
