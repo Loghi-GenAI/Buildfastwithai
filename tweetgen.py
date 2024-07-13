bed92b1072693a94d73567128a37806de8fc87b6
Loghi-GenAI <loghi.etl@gmail.com>
2024-07-13 07:33:45 +0530

    Add files via upload

---
+++ tweetgen.py
@@ -0,0 +1,16 @@
+import streamlit as st
+import langchain
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI as CGGA
+os.environ['GOOGLE_API_KEY']  = st.secrets["OPEN_API_KEY"]
+
+st.title("Tweet Generator")
+st.subheader("Generate Tweets on any topic")
+topic = st.text_input("Topic")
+number = st.number_input("No. of tweets",min_value=1,max_value=10,step=1,value=1)
+
+if st.button("Generate"):
+    prompt = f"Give me {number} of tweets on {topic}"
+    gemini_model = CGGA(model="gemini-1.5-flash-latest")
+    response=gemini_model.invoke(prompt)
+    st.write(response.content)
